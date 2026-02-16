10a32ef3b1336975baa4c612f274646b998d95a9
nethu0123 <nethu012345@gmail.com>
2026-02-16 22:32:49 +0530

    login page created

---
+++ ideate-web-app/src/pages/Login.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import "./Login.css";
+
+export default function LoginPage() {
+
+  const [role, setRole] = useState("employee");
+  const [showPass, setShowPass] = useState(false);
+
+  const idLabel = role === "admin" ? "Admin ID" : "Employee ID";
+const idPlaceholder = role === "admin"
+  ? "Enter Admin ID"
+  : "Enter Employee ID";
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Login as ${role}`);
+  };
+
+  return (
+    <div className="page">
+
+      <div className="card">
+
+        {/* LEFT SECTION */}
+        <div className="left">
+          <div className="brand">💡 Ideate</div>
+          <p className="tagline">
+            Think • Share • Grow
+          </p>
+
+          <h1>
+            Where <span className="greatmindstext">Great Minds</span> Meet
+          </h1>
+          <p className="logintext">Unlocking the collective intelligence of our organization to drive meaningful innovation and sustainable growth.</p>
+
+          
+        </div>
+
+
+        {/* RIGHT SECTION */}
+        <div className="right">
+
+          <h2>Welcome to Ideate</h2>
+
+          {/* Sliding Switch */}
+          <div className="switch">
+            <div className={`slider ${role}`} />
+            <button onClick={()=>setRole("employee")}>Employee</button>
+            <button onClick={()=>setRole("admin")}>Admin</button>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <label>{idLabel}</label>
+            <input type="text" placeholder={idPlaceholder} required />
+
+            <label>Password</label>
+            <div className="password-box">
+              <input
+                type={showPass ? "text" : "password"}
+                placeholder="Enter password"
+                required
+              />
+            </div>
+
+            
+
+            <button className="login-btn">Login</button>
+
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
